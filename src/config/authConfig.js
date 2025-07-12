93208f2f2415784bf7a6e88b586d486697ea6b51
Brad Blanchard <brad.blanchard@dmre.com>
2025-07-12 01:07:39 -0500

    Configure AWS Cognito for PROD and add authConfig.js

---
+++ src/config/authConfig.js
@@ -0,0 +1,7 @@
+export const cognitoAuthConfig = {
+  authority: "https://wellsaid-prod-prod.auth.us-east-2.amazoncognito.com",
+  client_id: "7tgjtfqfjc7ed52va2r9bnq1np",
+  redirect_uri: "https://app.wellsaidapp.com/",
+  response_type: "code",
+  scope: "email openid profile",
+};
