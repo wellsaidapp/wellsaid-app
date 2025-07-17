5fd808cde3e01d04f4170dd91578b0dab1964a04
Brad Blanchard <brad.blanchard@dmre.com>
2025-07-17 11:14:01 -0500

    Updated static insights json with actual ids from RDS

--- src/constants/insights.js
+++ src/constants/insights.js
@@ -4,9 +4,9 @@ export const INSIGHTS = [
     prompt: "What’s a principle you and your spouse have committed to in your marriage?",
     response: "We’ve committed to not use the words 'always' or 'never' when describing one another in a negative way. It helps us stay constructive and avoid exaggeration in conflict.",
     date: '2025-07-02',
-    collections: ['relationships', 'sage-summer-intensive', 'cohens-birthday'],
-    userId: '1',
-    personIds: [1, 2],
+    collections: ['f7529d28-eb61-48bd-8287-f443cc4d65cd', 'd011089d-dcb4-483e-aa4c-4f034e58cadc', 'fcdfd360-c3a2-41fe-8828-85c738e5240e'],
+    userId: '32cbd106-1886-47e9-8466-720df8ee97ce',
+    personIds: ['f607fe87-31ae-4048-bc50-7fc07ffe4047', 'a749593f-1425-4eda-8d15-87da50b4088e'],
     bookIds: []
   },
   {
@@ -14,9 +14,9 @@ export const INSIGHTS = [
     prompt: "Has your mindset around parenting changed over time?",
     response: "I used to think kids made life messier. But the mindset shift I’ve made is this: my kids *are* my nice things. They are infinitely more valuable than anything they might break.",
     date: '2025-07-02',
-    collections: ['parenting', 'personal-growth', 'cohens-birthday'],
-    userId: '1',
-    personIds: [1, 2],
+    collections: ['9b902abc-997a-47fc-8191-4205e61fda58', '79e1f497-988e-4405-952c-f952c95bb8e8', 'fcdfd360-c3a2-41fe-8828-85c738e5240e'],
+    userId: '32cbd106-1886-47e9-8466-720df8ee97ce',
+    personIds: ['f607fe87-31ae-4048-bc50-7fc07ffe4047', 'a749593f-1425-4eda-8d15-87da50b4088e'],
     bookIds: []
   },
   {
@@ -24,9 +24,9 @@ export const INSIGHTS = [
     prompt: "How do you handle difficult or uncomfortable conversations with your children?",
     response: "If something feels uncomfortable to bring up, I take that as a signal that it’s probably worth talking about. Those are often the conversations that matter most.",
     date: '2025-06-02',
-    collections: ['parenting', 'personal-growth'],
-    userId: '1',
-    personIds: [1],
+    collections: ['9b902abc-997a-47fc-8191-4205e61fda58', '79e1f497-988e-4405-952c-f952c95bb8e8'],
+    userId: '32cbd106-1886-47e9-8466-720df8ee97ce',
+    personIds: ['f607fe87-31ae-4048-bc50-7fc07ffe4047'],
     bookIds: []
   },
   {
@@ -34,8 +34,8 @@ export const INSIGHTS = [
     prompt: "What makes you proud as a parent?",
     response: "When I see my kids loving each other — like my teenage son sitting down to play with his 4-year-old brother — it makes me incredibly proud. It shows they’re choosing to invest their time in others.",
     date: '2025-04-02',
-    collections: ['parenting', 'family'],
-    userId: '1',
+    collections: ['9b902abc-997a-47fc-8191-4205e61fda58', '2b5f9769-f6f6-460d-b660-c9729b80ab07'],
+    userId: '32cbd106-1886-47e9-8466-720df8ee97ce',
     personIds: [],
     bookIds: []
   },
@@ -44,8 +44,8 @@ export const INSIGHTS = [
     prompt: "How has your relationship with your teenagers evolved?",
     response: "As my kids have become teens, I’ve realized they’re capable of doing adult things. I now go on intentional walks with my teenage son — just like I would with a close friend.",
     date: '2025-03-02',
-    collections: ['parenting', 'personal-growth'],
-    userId: '1',
+    collections: ['9b902abc-997a-47fc-8191-4205e61fda58', '79e1f497-988e-4405-952c-f952c95bb8e8'],
+    userId: '32cbd106-1886-47e9-8466-720df8ee97ce',
     personIds: [],
     bookIds: []
   },
@@ -54,8 +54,8 @@ export const INSIGHTS = [
     prompt: "What’s helped you grow the most personally?",
     response: "I used to focus only on things I knew I could accomplish. But I’ve grown the most when there was a risk of failure. The process, even when it doesn’t work out, expands what I believe I’m capable of.",
     date: '2025-02-01',
-    collections: ['personal-growth'],
-    userId: '1',
+    collections: ['79e1f497-988e-4405-952c-f952c95bb8e8'],
+    userId: '32cbd106-1886-47e9-8466-720df8ee97ce',
     personIds: [],
     bookIds: []
   },
@@ -64,8 +64,8 @@ export const INSIGHTS = [
     prompt: "How do you process setbacks or failures?",
     response: "Setbacks often come down to perspective and expectations. Sometimes it’s not the event itself, but my unrealistic or unspoken expectations that made it feel like a failure. Adjusting my frame helps me move forward.",
     date: '2025-01-05',
-    collections: ['personal-growth', 'work'],
-    userId: '1',
+    collections: ['79e1f497-988e-4405-952c-f952c95bb8e8', '479a4942-9181-4b2e-aeb0-27bb2635388e'],
+    userId: '32cbd106-1886-47e9-8466-720df8ee97ce',
     personIds: [],
     bookIds: []
   },
@@ -74,8 +74,8 @@ export const INSIGHTS = [
     prompt: "What have you learned about providing for your family?",
     response: "Providing financially is important, but it doesn’t compensate for being present. Proximity isn’t the same as presence. My kids want *me*, not just my support or paycheck.",
     date: '2025-06-30',
-    collections: ['parenting', 'personal-growth'],
-    userId: '1',
+    collections: ['9b902abc-997a-47fc-8191-4205e61fda58', '79e1f497-988e-4405-952c-f952c95bb8e8'],
+    userId: '32cbd106-1886-47e9-8466-720df8ee97ce',
     personIds: [],
     bookIds: []
   },
@@ -84,9 +84,9 @@ export const INSIGHTS = [
     prompt: "Do you have any rules that guide how you show up at home or at work?",
     response: "If I ever ask myself, 'Would it be lazy of me not to do this?' and the answer is yes — then I do it. Whether it’s picking something up at home or stepping in to help at work, that question keeps me accountable.",
     date: '2025-06-15',
-    collections: ['personal-growth', 'work'],
-    userId: '1',
-    personIds: [2],
+    collections: ['79e1f497-988e-4405-952c-f952c95bb8e8', '479a4942-9181-4b2e-aeb0-27bb2635388e'],
+    userId: '32cbd106-1886-47e9-8466-720df8ee97ce',
+    personIds: ['a749593f-1425-4eda-8d15-87da50b4088e'],
     bookIds: []
   }
 ];
